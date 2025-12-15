12a1fbb841e416630552363757b4605ce89c8903
hmstech11@gmail.com <146432351+hafizsameer11@users.noreply.github.com>
2025-12-15 17:56:02 +0500

    Updated.

--- src/validators/ad.validators.ts
+++ src/validators/ad.validators.ts
@@ -74,7 +74,7 @@ export const createAdValidator = z
   .object({
     body: z
       .object({
-        title: z.string().min(3, "Title must be at least 3 characters"),
+        title: z.string().min(1, "Title is required"),
         type: AdTypeEnum,
         imageUrl: z
           .string()
@@ -124,7 +124,7 @@ export const updateAdValidator = z
     }),
     body: z
       .object({
-        title: z.string().min(3, "Title must be at least 3 characters").optional(),
+        title: z.string().min(1, "Title is required").optional(),
         type: AdTypeEnum.optional(),
         imageUrl: z
           .string()
